662fa69f66188d13c57576c434e46fae75349073
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-06 14:58:12 +0200

    feat(): add Header module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { Header } from './modules/Header';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Header title="Lista zadań" />
   );
 }
 
