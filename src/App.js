d24d63affb599c7dd4baf4a6323eb755cfe02087
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-06 15:56:43 +0200

    feat(): add Form module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from './modules/Container';
 import { Header } from './modules/Header';
 import { Section } from './modules/Section';
+import { Form } from './modules/Form';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
+        body={<Form />}
       />
       <Section
         title="Lista zadań"
---
+++ src/modules/Form/Form.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Form = () => (
+  <form>
+    <input placeholder="Co jest do zrobienia?" />
+    <button>Dodaj zadanie</button>
+  </form>
+);
