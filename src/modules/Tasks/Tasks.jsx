485d7201c36c12e36fbf53942e764f005199097c
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-06 20:45:09 +0200

    feat(Tasks): add some tasks

--- src/modules/Tasks/Tasks.jsx
+++ src/modules/Tasks/Tasks.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
-export const Tasks = () => (
-  <ul></ul>
-);
+export const Tasks = () => {
+  const tasks = [
+    {
+      id: 1,
+      content: "przepisać to do List",
+      done: false
+    },
+    {
+      id: 2,
+      content: "przepisać currecy office",
+      done: true
+    }
+  ]
+  return (
+    < ul >
+      {tasks.map(({ id, content }) => (
+        <li key={id}>{content}</li>
+      ))}
+    </ul>
+  );
+};
