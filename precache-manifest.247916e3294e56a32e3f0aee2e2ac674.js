3d9383e768e2672ac76b40c079628d3a764c80d5
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-07 23:35:35 +0200

    Updates

---
+++ precache-manifest.247916e3294e56a32e3f0aee2e2ac674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df579ad774b57d321dea7f7ea3585e3",
+    "url": "/to_do_list_react/index.html"
+  },
+  {
+    "revision": "96e81e28237454dde3ab",
+    "url": "/to_do_list_react/static/css/main.bbf45f3e.chunk.css"
+  },
+  {
+    "revision": "ec3ed769b2f67e4a44f6",
+    "url": "/to_do_list_react/static/js/2.565cbe75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_do_list_react/static/js/2.565cbe75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e81e28237454dde3ab",
+    "url": "/to_do_list_react/static/js/main.4ae6cce2.chunk.js"
+  },
+  {
+    "revision": "f317b4eecac76b52faed",
+    "url": "/to_do_list_react/static/js/runtime-main.6ae72c20.js"
+  }
+]);
