cc6c35b6e89cdb1da25e18538ecbc1451a46e53b
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-12 12:10:58 +0200

    refactor(Form): change name

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,14 +3,14 @@ import { FormContener, FormInput, FormButton } from './styled';
 
 export const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
-  const formFocus = useRef(null);
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     const trimmedTaskContent = newTaskContent.trim();
     event.preventDefault();
     if (trimmedTaskContent.length > 0) {
       addNewTask(trimmedTaskContent);
-      setNewTaskContent("")
+      setNewTaskContent("");
     }
     return;
   };
@@ -18,12 +18,12 @@ export const Form = ({ addNewTask }) => {
   return (
     <FormContener onSubmit={onFormSubmit}>
       <FormInput
-        ref={formFocus}
+        ref={inputRef}
         placeholder="Co jest do zrobienia?"
         value={newTaskContent}
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
-      <FormButton onClick={() => formFocus.current.focus()}>Dodaj zadanie</FormButton>
+      <FormButton onClick={() => inputRef.current.focus()}>Dodaj zadanie</FormButton>
     </FormContener>
   )
 };
