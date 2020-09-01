52d7e315bb2682f858e9403a61bba6444476f0de
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-01 22:27:45 +0200

    style(): add Global style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./index.css";
-import { App } from "./App";
+import { ThemeProvider } from "styled-components";
 import store from "./store";
+import { GlobalStyle } from "./GlobalStyle";
+import { App } from "./App";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
