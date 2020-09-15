b0c2beb911f3130ebcb1e2ffa354684de86689e9
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-15 14:45:08 +0200

    feat (): Add a task subpage

---
+++ src/features/Tasks/TaskPage/TaskPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container } from "../../../common/Container";
+import { Section } from "../../../common/Section";
+import { Header } from "../../../common/Header";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+export const TaskPage = () => {
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
+
+  return (
+    <Container>
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Nie znaleziono zadania 😥"}
+        body={
+          task ? (
+            <>
+              {task.done
+                ? "Zadanie zostało ukończone. Gratuluję 😉"
+                : "Zadanie nie zostało jeszcze ukończone 😥"}
+            </>
+          ) : (
+            ""
+          )
+        }
+      />
+    </Container>
+  );
+};
